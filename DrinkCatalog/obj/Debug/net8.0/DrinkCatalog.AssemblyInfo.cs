bf2d17e472181c196918be74445ef3dfbceb5ad8
Dmitriy Sannikov <omeom@mail.ru>
2024-09-30 17:24:04 +0400

    Delete some js lib

--- DrinkCatalog/obj/Debug/net8.0/DrinkCatalog.AssemblyInfo.cs
+++ DrinkCatalog/obj/Debug/net8.0/DrinkCatalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrinkCatalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8592d170174792d8cd314e648acf016d384dc774")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d39889c7209d31e0f001818ffa915c4848ed3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrinkCatalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrinkCatalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
